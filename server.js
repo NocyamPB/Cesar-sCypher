dc3b252ef6b2f794378fe6c3712d68d9364d891a
Maycon Santos Barbieri <maycon.barbieri@hotmail.com>
2019-05-24 17:26:00 -0400

    Atualizacao organizacional das pastas

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/views'));
 
 app.get('/', function(req, res){
-    res.render('index');
+    res.render('pages/index');
 });
 
 app.listen(port, function(err){
