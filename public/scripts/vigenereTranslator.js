b5da1d8be455fb9312c9f7e7107000fc08864503
Maycon Santos Barbieri <maycon.barbieri@hotmail.com>
2019-06-25 15:07:10 -0400

    Adicao do script de Vigenere

--- public/scripts/vigenereTranslator.js
+++ public/scripts/vigenereTranslator.js
@@ -2,12 +2,20 @@ const alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
 
 function criptografar(){
     var mensagem = document.querySelector('#txt-mensagem').value.toLowerCase();
-    var chave = parseInt(document.querySelector('#chave').value);
+    var chave = document.querySelector('#chave').value;
+    var chaveCompleta = "";
     var resultado = "";
+    for (let i = 0; i <= mensagem.length; i++) {
+        for (let j = 0; j <= chave.length; j++) {
+            chaveCompleta += chave.charAt(j);
+            i++;
+        }
+    };
     for(let i = 0; i <= mensagem.length; i++){
         let numLetra = verificarLetra(mensagem.charAt(i));
-        if(numLetra!=null){
-            let novaLetra = numLetra + chave;
+        let numChave = verificarLetra(chaveCompleta.charAt(i));
+        if(numLetra!=null && numChave!=null){
+            let novaLetra = numLetra + numChave;
             if(novaLetra>=26){
                 novaLetra-=alfabeto.length;
             }
@@ -36,12 +44,20 @@ function verificarLetra(letra){
 
 function descriptografar(){
     var mensagem = document.querySelector('#txt-mensagem').value.toLowerCase();
-    var chave = parseInt(document.querySelector('#chave').value);
+    var chave = document.querySelector('#chave').value;
+    var chaveCompleta = "";
     var resultado = "";
+    for (let i = 0; i <= mensagem.length; i++) {
+        for (let j = 0; j <= chave.length; j++) {
+            chaveCompleta += chave.charAt(j);
+            i++;
+        }
+    };
     for(let i = 0; i <= mensagem.length; i++){
         let numLetra = verificarLetra(mensagem.charAt(i));
-        if(numLetra!=null){
-            let novaLetra = numLetra - chave;
+        let numChave = verificarLetra(chaveCompleta.charAt(i));
+        if(numLetra!=null && numChave!=null){
+            let novaLetra = numLetra - numChave;
             if(novaLetra<=-1){
                 novaLetra+=alfabeto.length;
             }
